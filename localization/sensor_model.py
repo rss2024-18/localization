fixed the normalization

localization/sensor_model.py
@@ -103,27 +103,6 @@ class SensorModel:
 
         self.sensor_model_table = self.sensor_model_table.T
 
-        # z_max = float('inf') # not sure where z_max is defined
-        # d = 7 # or this guy
-        # E = 0.1 # this guy too
-
-        # # Compute sensor model probabilities
-        # for i in range(self.table_width):
-        #     normalization = 0
-        #     for j in range(self.table_width):
-        #         z = j-i
-        #         p_hit = self.alpha_hit * 1/((2 * np.pi * self.sigma_hit ** 2)**(1/2)) * np.exp(-((z-d)**2) / (2 * self.sigma_hit ** 2)) if (0 <= z <= z_max ) else 0
-        #         p_short = self.alpha_short *  2 / (d * (1 - z/d)) if (0 <= z <= d) else 0
-        #         p_max = self.alpha_max / E if (z_max - E <= z <= z_max) else 0
-        #         p_rand = self.alpha_rand / z_max if (0 <= z <= z_max ) else 0
-
-        #         probs = sum([p_hit, p_short, p_max, p_rand])
-        #         normalization += probs
-        #         self.sensor_model_table[j, i] = probs
-
-        #     self.sensor_model_table[:, i] /= normalization
-
-
     def evaluate(self, particles, observation):
         """
         Evaluate how likely each particle is given
@@ -145,21 +124,28 @@ class SensorModel:
                 given the observation and the map.
         """
 
-        num_particles = particles.shape[0]
         if not self.map_set:
-            return np.ones(num_particles) / num_particles # uniform distribution if map is not set
-        
-        scans = self.scan_sim.scan(particles) # N particles x M laser beams per particle
+            return np.ones(len(particles)) / len(particles)  # Uniform distribution if map is not set
 
-        probabilities = np.ones(num_particles)
+        scans = self.scan_sim.scan(particles)
+        num_particles = scans.shape[0]
+        weights = np.zeros(num_particles)
+
         for i, particle_scan in enumerate(scans):
-            # assume observation has already been downsampled => should be M long
-            for observed_range_m, expected_range_m in zip(observation, particle_scan):
-                observed_range_px = np.clip(int(observed_range_m / (self.resolution * self.lidar_scale_to_map_scale)), 0, self.table_width-1)
-                expected_range_px = np.clip(int(expected_range_m / (self.resolution * self.lidar_scale_to_map_scale)), 0, self.table_width-1)
-                probabilities[i] *= self.sensor_model_table[observed_range_px][expected_range_px]
-        
-        return probabilities
+            weight = 1.0
+            for observed_range, expected_range in zip(observation, particle_scan):
+                prob_table_row = self.sensor_model_table[int(observed_range)]
+                weight *= prob_table_row[int(expected_range)]
+            weights[i] = weight
+    
+        weights = np.power(weights, 1/3)
+        tot = np.sum(weights)
+        weights = weights/tot
+
+        return weights
+
+
+        ####################################
 
     def map_callback(self, map_msg):
         # Convert the map to a numpy array